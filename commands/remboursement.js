a70b4ca37367abd1c95cb84a84eb5f806117fc0f
unknown <lydiepluvinage@yahoo.fr>
2022-03-11 15:35:26 +0100

    Ajout de la commande pour le remboursement

---
+++ commands/remboursement.js
@@ -0,0 +1,6 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+  name: "remboursement",
+  description: "R2D2, donne nous les moyens de rembourser Lydie stp",
+};
