3b734bfd23e2bbffe083c7c0befd6207522eadd3
Lydie Pluvinage <lydiepluvinage@yahoo.fr>
2022-03-15 22:13:27 +0100

    Horoscope. Need to add differents zodiac signs now

---
+++ commands/horoscope.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: 'horoscope',
+  description: "Donne l'horoscope du jour'",
+  options: [
+    { name: 'Bélier', description: 'Pour les béliers', type: 1 },
+    { name: 'Taureau', description: 'Pour les taureaux', type: 1 },
+    { name: 'Gémeaux', description: 'Pour les gémeaux', type: 1 },
+    { name: 'Cancer', description: 'Pour les cancers', type: 1 },
+    { name: 'Lion', description: 'Pour les lions', type: 1 },
+    { name: 'Vierge', description: 'Pour les vierges', type: 1 },
+    { name: 'Balance', description: 'Pour les balances', type: 1 },
+    { name: 'Scorpion', description: 'Pour les scorpions', type: 1 },
+    { name: 'Sagittaire', description: 'Pour les sagittaires', type: 1 },
+    { name: 'Capricorne', description: 'Pour les capricornes', type: 1 },
+    { name: 'Verseau', description: 'Pour les verseaux', type: 1 },
+    { name: 'Poissons', description: 'Pour les poissons', type: 1 },
+  ],
+};
--- commands/meteo.js
+++ commands/meteo.js
@@ -1,6 +1,4 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
-
 module.exports = {
-  name: "meteo",
-  description: "Donne la météo pour les 3 prochains jours sur Anglet",
+  name: 'meteo',
+  description: 'Donne la météo pour les 3 prochains jours sur Anglet',
 };
